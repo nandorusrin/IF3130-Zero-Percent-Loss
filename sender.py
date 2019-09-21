d12da09ced478b4d31a8e63c78fd6b3bd9b03eae
ikraduya <ikraduya@gmail.com>
2019-09-21 10:52:28 +0700

    receiver sending ack & sender could simulate time out

--- sender.py
+++ sender.py
@@ -2,7 +2,17 @@ import socket
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
-Message = "Hello, Server"
+Message = "Hello"
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-clientSock.sendto(Message.encode(), (UDP_IP_ADDRESS, UDP_PORT_NO))
+
+try:
+  clientSock.sendto(Message.encode(), (UDP_IP_ADDRESS, UDP_PORT_NO))
+  clientSock.settimeout(1)
+
+  data, attr = clientSock.recvfrom(1024)
+  print("received ack:", data)
+
+except socket.timeout:
+  print("Timeout reached")
+  clientSock.close()
