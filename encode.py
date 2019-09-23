2e2c9b34aae763508b80da08c6c9c9123b2b99a5
Arvin <13517124@std.stei.itb.ac.id>
2019-09-23 12:34:02 +0700

    fungsi get_ID dan get_type udah bisa jalan

--- encode.py
+++ encode.py
@@ -9,26 +9,36 @@ def concat_type_ID(type, id):
 	return int(result).to_bytes(1, byteorder="little", signed=False)
 
 #keluarin integer ID dari input byte
+#DONE
 def get_ID(byte):
 	temp = bin(int.from_bytes(byte, byteorder="little", signed=False))
-	return int(temp[-4:])
+	return int("0b"+temp[-4:], 2)
+
 #keluarin integer TYPE dari input byte
+#DONE
 def get_type(byte):
-	return int(byte[0:-4], 2)
+	temp = bin(int.from_bytes(byte, byteorder="little", signed=False))
+
+	return int(temp[0:-4], 2)
 
 #encode length ke bytes
 #DEF ini done
 def encode_length(length):
 	return length.to_bytes(2, byteorder="little", signed=False)
-	
-
-message = "hello, world!"
 
 f_img = open("stark.jpg", "rb")
 num = list(f_img.read())
 print(len(num))
 f_img.close()
 
-eg = concat_type_ID(6, 4)
-print(int.from_bytes(eg, byteorder="little", signed=False))
+
+#CONTOH
+tipe = 3
+ID = 5
+LENGTH = 4239
+eg = concat_type_ID(tipe, ID)
+print("Ukuran concatenate   : ", len(eg), " byte")
+print(get_type(eg))
 print(get_ID(eg))
+print("Hasil encode LENGTH  : ", encode_length(LENGTH))
+print("Ukuran encode LENGTH : ", len(encode_length(LENGTH)), " byte(s)")
